17d69b284c4a7448db183a2f11340a9d6231c40e
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-04-27 16:49:08 +0300

    https://blazemeter.atlassian.net/browse/BPC-112

--- src/main/java/com/blazemeter/jmeter/testinfo/TestInfoChecker.java
+++ src/main/java/com/blazemeter/jmeter/testinfo/TestInfoChecker.java
@@ -2,7 +2,6 @@ package com.blazemeter.jmeter.testinfo;
 
 import com.blazemeter.jmeter.testexecutor.BmTestManager;
 import com.blazemeter.jmeter.utils.BlazemeterApi;
-import com.blazemeter.jmeter.utils.BmLog;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,40 +10,19 @@ import com.blazemeter.jmeter.utils.BmLog;
  * Time: 5:39 PM
  * To change this template use File | Settings | File Templates.
  */
-public class TestInfoChecker extends Thread {
+public class TestInfoChecker implements Runnable {
     private String testId;
 
     protected TestInfoChecker(String testId) {
         this.testId = testId;
     }
 
-    public String getTestId() {
-        return testId;
-    }
-
-    public void setTestId(String testId) {
-        this.testId = testId;
-    }
 
     @Override
     public void run() {
         BmTestManager bmTestManager = BmTestManager.getInstance();
-        try {
-            if (isInterrupted()) {
-                throw new InterruptedException();
-            }
-
-            TestInfo testInfo = BlazemeterApi.getInstance().getTestRunStatus(bmTestManager.getUserKey(),
-                    this.testId, true);
-            bmTestManager.setTestInfo(testInfo);
-
-        } catch (InterruptedException e) {
-            BmLog.debug("TestInfoChecker was interrupted during sleeping");
-            return;
-        } finally {
-            if (isInterrupted()) {
-                return;
-            }
-        }
+        TestInfo testInfo = BlazemeterApi.getInstance().getTestRunStatus(bmTestManager.getUserKey(),
+                this.testId, true);
+        bmTestManager.setTestInfo(testInfo);
     }
 }
--- src/main/java/com/blazemeter/jmeter/testinfo/TestInfoController.java
+++ src/main/java/com/blazemeter/jmeter/testinfo/TestInfoController.java
@@ -2,6 +2,7 @@ package com.blazemeter.jmeter.testinfo;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,21 +13,23 @@ import java.util.concurrent.TimeUnit;
  * To change this template use File | Settings | File Templates.
  */
 public class TestInfoController {
-    private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private static ScheduledFuture<?> task;
 
     private TestInfoController() {
     }
 
     public static void start(String testId) {
-        final TestInfoChecker testInfoChecker = new TestInfoChecker(testId);
-        if (scheduler.isShutdown()) {
-            scheduler.scheduleAtFixedRate(testInfoChecker, 1, 30, TimeUnit.SECONDS);
+        if (task == null || task.isDone()) {
+            final TestInfoChecker testInfoChecker = new TestInfoChecker(testId);
+            task = scheduler.scheduleAtFixedRate(testInfoChecker, 1, 30, TimeUnit.SECONDS);
+
         }
     }
 
     public static void stop() {
-        if (!scheduler.isShutdown()) {
-            scheduler.shutdown();
+        if (task != null && !task.isDone()) {
+            task.cancel(true);
         }
     }
 }
