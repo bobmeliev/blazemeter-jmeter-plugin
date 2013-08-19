167dd9b08d9d36dd0e09a569e77ecebd05239dd4
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-08-19 19:22:42 +0300

    update

--- src/main/java/com/blazemeter/jmeter/results/SamplesUploader.java
+++ src/main/java/com/blazemeter/jmeter/results/SamplesUploader.java
@@ -18,7 +18,6 @@ import java.util.concurrent.TimeUnit;
 public class SamplesUploader {
     private static ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private static ScheduledFuture<?> task;
-    private static SamplesUploader samplesUploader = null;
     private static Thread samplesUploaderThread = null;
     private static final int batchSize = 50;
     private static final SamplesQueue samplesQueue = new SamplesQueue(batchSize);
@@ -38,8 +37,8 @@ public class SamplesUploader {
                         long begin = System.currentTimeMillis();
                         List<String> batch = samplesQueue.take((int) delay);
                         send(batch);
-                        long millisOfCurrentItration = System.currentTimeMillis() - begin;
-                        delay = MAX_DELAY - millisOfCurrentItration;
+                        long millisOfCurrentIteration = System.currentTimeMillis() - begin;
+                        delay = MAX_DELAY - millisOfCurrentIteration;
                         if (delay < 0) {
                             // если отсылка заняла больше 30 секунд
                             delay = 0;
@@ -55,6 +54,7 @@ public class SamplesUploader {
     }
 
     public static void stop() {
+
         task.cancel(true);
         /*
         1.Stop checking time;
@@ -63,24 +63,16 @@ public class SamplesUploader {
         4. Send them to server;
         5. Die.
          */
-        /*
-        TODO
-        1.QueueChecker;
-        2.JTL2JSON converter;
-        3.JSON2String converter
-         */
+
     }
 
     public static void addSample(String jsonString) {
-       /*
-       1.Add sample to samplesQueue
-        */
-    }
+        try {
+            samplesQueue.put(jsonString);
 
-    public static void addSamples(List<String> jsonStrings) {
-       /*
-       1.Add List of samples to samplesQueue
-        */
+        } catch (InterruptedException ie) {
+            BmLog.error("Failed to add sample to uploader: Interruptedexception");
+        }
     }
 
     private static void send(List<String> batch) {
