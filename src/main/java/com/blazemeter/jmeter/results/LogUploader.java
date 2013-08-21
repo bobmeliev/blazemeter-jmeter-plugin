d6377bd90c041267d78850a77e3dbe70210a3023
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-08-21 21:55:44 +0300

    refactoring JMeterLogsUploader.java

--- src/main/java/com/blazemeter/jmeter/results/LogUploader.java
+++ src/main/java/com/blazemeter/jmeter/results/LogUploader.java
@@ -1,34 +1,113 @@
 package com.blazemeter.jmeter.results;
 
 import com.blazemeter.jmeter.testexecutor.BmTestManager;
+import com.blazemeter.jmeter.testexecutor.Constants;
 import com.blazemeter.jmeter.testinfo.TestInfo;
 import com.blazemeter.jmeter.utils.BmLog;
+import com.blazemeter.jmeter.utils.Utils;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.logging.LoggingManager;
 
-/**
- * Created with IntelliJ IDEA.
- * User: dzmitrykashlach
- * Date: 8/14/13
- * Time: 2:19 PM
- * To change this template use File | Settings | File Templates.
- */
-public class LogUploader implements Runnable {
-    private final String logName;
-    private final String data;
-    private String dataType;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
+public class LogUploader {
+    private static LogUploader instance;
+    private final HashMap<String, String> log_files = new HashMap<String, String>();
+    private final HashMap<String, BufferedReader> logFileReaders = new HashMap<String, BufferedReader>();
+    private boolean uploadFinished;
+    private boolean isRunning = false;
 
-    public LogUploader(String logName, String data, String dataType) {
-
-        this.logName = logName;
-        this.data = data;
-        this.dataType = dataType;
+    private LogUploader() {
     }
 
-    @Override
-    public void run() {
+    public static LogUploader getInstance() {
+        if (instance == null)
+            instance = new LogUploader();
+        return instance;
+    }
+
+    public void startListening() {
+        if (isRunning)
+            return;
+
+        log_files.put("jmeter_log", JMeterUtils.getPropDefault(LoggingManager.LOG_FILE, "jmeter.log"));
+        if (Utils.isJMeterServer()) {
+            log_files.put("jmeter_server_log", Constants.JMETER_SERVER_LOG_FILENAME);
+        }
+        for (Map.Entry<String, String> entry : log_files.entrySet()) {
+            StringBuilder filename = new StringBuilder(entry.getValue());
+            if (!new File(filename.toString()).exists()) {
+                filename = new StringBuilder(JMeterUtils.getJMeterBinDir() + "/").append(filename);
+            }
+            try {
+                String host = Utils.getHostIP() + (Utils.isJMeterServer() ? "(jmeter-server)" : "");
+                BmLog.console("Log file path at host= " + host + " is: " + filename);
+
+                BufferedReader logBufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename.toString())));
+                logFileReaders.put(entry.getKey(), logBufferedReader);
+                uploadLog(filename.toString(), logBufferedReader);
+            } catch (FileNotFoundException fnfe) {
+                BmLog.error("Failed to find log file " + filename + ": " + fnfe.getMessage());
+            }
+        }
+        isRunning = true;
+    }
+
+
+    private void uploadLog(String logFilename, BufferedReader logReader) {
+        uploadFinished = false;
+        boolean last = true;
+        while (isRunning || last) {
+            StringBuilder buff = new StringBuilder(4096);
+            String line;
+            try {
+                while ((line = logReader.readLine()) != null) {
+                    buff.append(line);
+                    buff.append("\n");
+                }
+            } catch (IOException e) {
+                BmLog.error("Problems with reading " + logFilename, e);
+            }
+            if (buff.length() > 0) {
+                logFilename = logFilename.substring(logFilename.lastIndexOf(System.getProperty("file.separator")) + 1);
+                upload(Utils.getHostIP() + "_" + logFilename, buff.toString(), "log");
+            }
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ignored) {
+            }
+            last = !isRunning && !last;
+        }
+        uploadFinished = true;
+
+    }
+
+    public void stopListening() {
+        if (!isRunning)
+            return;
+
+        isRunning = false;
+        while (!this.uploadFinished) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignored) {
+            }
+        }
+        for (Map.Entry<String, BufferedReader> entry : logFileReaders.entrySet()) {
+            try {
+                entry.getValue().close();
+            } catch (Exception e) {
+                BmLog.error("Failed to close " + entry.getKey() + " reader");
+            }
+        }
+    }
+
+    private void upload(String logName, String data, String dataType) {
         TestInfo testInfo = BmTestManager.getInstance().getTestInfo();
         String testId = testInfo.getId();
-        BmLog.debug(String.format("Log uploader sending log:%s , %d bytes  ", this.logName, this.data.length()));
-        BmTestManager.getInstance().logUpload(testId, this.logName, this.data, this.dataType);
+        BmLog.debug(String.format("Log uploader sending log:%s , %d bytes  ", logName, data.length()));
+        BmTestManager.getInstance().logUpload(testId, logName, data, dataType);
     }
 }
--- src/main/java/com/blazemeter/jmeter/testexecutor/RemoteTestRunner.java
+++ src/main/java/com/blazemeter/jmeter/testexecutor/RemoteTestRunner.java
@@ -2,7 +2,7 @@ package com.blazemeter.jmeter.testexecutor;
 
 //~--- non-JDK imports --------------------------------------------------------
 
-import com.blazemeter.jmeter.results.JMeterLogsUploader;
+import com.blazemeter.jmeter.results.LogUploader;
 import com.blazemeter.jmeter.results.SamplesUploader;
 import com.blazemeter.jmeter.testinfo.Overrides;
 import com.blazemeter.jmeter.testinfo.TestInfo;
@@ -175,7 +175,7 @@ public class RemoteTestRunner extends ResultCollector implements SampleListener,
                 BmLog.debug("Opening test URL: " + url);
                 JMeterUtils.setProperty(Constants.TEST_URL_WAS_OPENED, "true");
             }
-            JMeterLogsUploader.getInstance().startListening();
+            LogUploader.getInstance().startListening();
             SamplesUploader.startUploading(callBackUrl);
         }
     }
@@ -193,7 +193,7 @@ public class RemoteTestRunner extends ResultCollector implements SampleListener,
         } else {
             SamplesUploader.stop();
         }
-        JMeterLogsUploader.getInstance().stopListening();
+        LogUploader.getInstance().stopListening();
         bmTestManager.stopTest();
     }
 
