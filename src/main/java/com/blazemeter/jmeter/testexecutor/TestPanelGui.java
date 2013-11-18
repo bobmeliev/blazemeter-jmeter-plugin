8a4a21582ca1a30d1676d654312f788bab4527d3
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-11-18 15:25:39 +0300

    BPC-207 fix

--- src/main/java/com/blazemeter/jmeter/testexecutor/TestPanelGui.java
+++ src/main/java/com/blazemeter/jmeter/testexecutor/TestPanelGui.java
@@ -785,6 +785,7 @@ public class TestPanelGui {
         runLocal.setSelected(isLocalRunMode);
         runRemote.setSelected(!isLocalRunMode);
         cloudPanel.setVisible(!isLocalRunMode);
+        jMeterPropertyPanel.setVisible(!isLocalRunMode);
     }
 
     protected void setTestInfo(TestInfo testInfo) {
