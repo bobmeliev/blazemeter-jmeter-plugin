362c8b3848d16c643362065230f8630b58c37bbc
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-11-21 12:43:13 +0300

    move VersionPanel to separate package

---
+++ src/main/java/com/blazemeter/jmeter/testexecutor/panels/VersionPanel.java
@@ -0,0 +1,13 @@
+package com.blazemeter.jmeter.testexecutor.panels;
+
+import javax.swing.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dzmitrykashlach
+ * Date: 11/21/13
+ * Time: 12:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class VersionPanel extends JPanel {
+}
