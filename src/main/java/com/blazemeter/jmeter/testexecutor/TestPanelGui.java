d0f003c67335375c2bd00e28bcbbf9dd9d27fa41
dzmitrykashlach <dzmitrykashlach@gmail.com>
2013-12-02 11:40:08 +0300

    BPC-226 fix

--- src/main/java/com/blazemeter/jmeter/testexecutor/TestPanelGui.java
+++ src/main/java/com/blazemeter/jmeter/testexecutor/TestPanelGui.java
@@ -953,7 +953,7 @@ public class TestPanelGui {
         userKeyTextField.setToolTipText("User key - can be found on your profile page , click \"?\" button for more info");
         panel2.add(userKeyTextField, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
         final JPanel panel3 = new JPanel();
-        panel3.setLayout(new GridLayoutManager(1, 5, new Insets(0, 0, 0, 0), -1, -1));
+        panel3.setLayout(new GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(panel3, new GridConstraints(2, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
         testIdTextField = new JTextField();
         testIdTextField.setEditable(false);
@@ -977,13 +977,13 @@ public class TestPanelGui {
         createNewButton.setPreferredSize(new Dimension(20, 20));
         createNewButton.setText("");
         createNewButton.setToolTipText("Create new test");
-        panel3.add(createNewButton, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_GROW, new Dimension(24, 21), null, null, 0, false));
+        panel3.add(createNewButton, new GridConstraints(0, 3, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_GROW, new Dimension(24, 21), null, null, 0, false));
         signUpToBlazemeterButton = new JButton();
         signUpToBlazemeterButton.setActionCommand("Sign up to BlazeMeter!");
         signUpToBlazemeterButton.setLabel("Sign up to BlazeMeter's free trial!");
         signUpToBlazemeterButton.setText("Sign up to BlazeMeter's free trial!");
         signUpToBlazemeterButton.setToolTipText("Register/Login to BlazeMeter site and find your User Key on a profile page.");
-        panel3.add(signUpToBlazemeterButton, new GridConstraints(0, 2, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, 1, GridConstraints.SIZEPOLICY_FIXED, new Dimension(250, -1), new Dimension(300, -1), new Dimension(300, -1), 0, false));
+        panel3.add(signUpToBlazemeterButton, new GridConstraints(0, 2, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, 1, GridConstraints.SIZEPOLICY_FIXED, new Dimension(250, -1), new Dimension(300, -1), new Dimension(300, -1), 0, false));
         final JPanel panel4 = new JPanel();
         panel4.setLayout(new GridLayoutManager(1, 4, new Insets(0, 0, 0, 0), -1, -1));
         panel1.add(panel4, new GridConstraints(1, 1, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
