07387dc6bb64fc8e9b3f98a6597da003786ad235
dzmitrykashlach <dzmitrykashlach@gmail.com>
2014-01-13 21:38:39 +0300

    TestUtils: parseTestInfo()

--- test/src/TestUtils.java
+++ test/src/TestUtils.java
@@ -1,15 +1,14 @@
 package src;
 
+import com.blazemeter.jmeter.entities.Overrides;
+import com.blazemeter.jmeter.entities.TestInfo;
+import com.blazemeter.jmeter.entities.TestStatus;
 import com.blazemeter.jmeter.entities.UserInfo;
 import com.blazemeter.jmeter.testexecutor.BmTestManager;
 import com.blazemeter.jmeter.utils.BmLog;
 import com.blazemeter.jmeter.utils.Utils;
 import junit.framework.Assert;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy;
-import org.apache.jmeter.samplers.SampleEvent;
-import org.apache.jmeter.samplers.SampleResult;
-import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.threads.JMeterContext;
 import org.apache.jmeter.threads.JMeterContextService;
 import org.json.JSONArray;
@@ -17,8 +16,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+
 
 /**
  * Created by dzmitrykashlach on 1/9/14.
@@ -27,9 +26,11 @@ public class TestUtils {
     private JMeterContext jmctx;
     private BmTestManager bmTestManager;
     private String LOCATIONS = System.getProperty("user.dir") + "/test/src/resources/locations.txt";
+    private String TEST_INFO = System.getProperty("user.dir") + "/test/src/resources/test-info.txt";
 
     @Before
     public void setUp() {
+
         jmctx = JMeterContextService.getContext();
         bmTestManager = BmTestManager.getInstance();
         String str = Utils.getFileContents(LOCATIONS);
@@ -76,30 +77,35 @@ public class TestUtils {
     public void getFileContents() {
         String actual_locations_content = "[{\"id\":\"SANDBOX_FT\",\"title\":\"SANDBOX\"},{\"id\":\"eu-west-1\",\"title\":\"EU West (Ireland)\"},{\"id\":\"us-east-1\",\"title\":\"US East (Virginia)\"},{\"id\":\"us-west-1\",\"title\":\"US West (N.California)\"},{\"id\":\"us-west-2\",\"title\":\"US West (Oregon)\"},{\"id\":\"ap-southeast-1\",\"title\":\"Asia Pacific (Singapore)\"},{\"id\":\"ap-southeast-2\",\"title\":\"Australia (Sydney)\"},{\"id\":\"ap-northeast-1\",\"title\":\"Japan (Tokyo)\"},{\"id\":\"sa-east-1\",\"title\":\"South America (San Paulo)\"},{\"id\":\"529c83f6bda5edfe438bdbc8\",\"title\":\"DZMITRY\"}]";
         String expected_locations_content = StringUtils.strip(Utils.getFileContents(LOCATIONS));
-
         Assert.assertEquals(expected_locations_content, actual_locations_content);
     }
 
     @Test
-    @Ignore
-    public void getJSONObject() {
-        HTTPSamplerProxy sampler = new HTTPSamplerProxy("HTTPSampler");
-
-        sampler.setProperty(TestElement.GUI_CLASS, "org.apache.jmeter.protocol.http.control.gui.HttpTestSampleGui");
-        sampler.setProperty(TestElement.TEST_CLASS, "org.apache.jmeter.protocol.http.sampler.HTTPSamplerProxy");
-        sampler.setDomain("google.com");
-        sampler.setPath("/");
-        sampler.setProtocol("http");
-        sampler.setMethod("GET");
-
-        jmctx.setCurrentSampler(sampler);
-        SampleResult result = sampler.sample();
-        SampleEvent event = new SampleEvent(result, "ThreadGroup");
-        JSONObject jo = Utils.getJSONObject(event);
-        /* TODO
-        1.Start stub web-server
-        2.Send request to this server(server takes document from file and sends back)
-        3.Compare with JSON which was loaded from file
-        */
+    public void parseTestInfo() {
+        String str = Utils.getFileContents(TEST_INFO);
+        JSONObject testInfoJO = null;
+        TestInfo ti_actual = new TestInfo();
+        TestInfo ti_expected = new TestInfo();
+        ti_expected.setId("411711");
+        ti_expected.setName("load_test");
+        ti_expected.setStatus(TestStatus.NotRunning);
+        ti_expected.setNumberOfUsers(690);
+        ti_expected.setLocation("us-west-2");
+        Overrides overrides = new Overrides(0, -1, 0, 230);
+        ti_expected.setOverrides(overrides);
+        ti_expected.setType("jmeter");
+        try {
+            testInfoJO = new JSONObject(str);
+            ti_actual = Utils.parseTestInfo(testInfoJO);
+        } catch (JSONException e) {
+            BmLog.error("Failed to construct TestInfoJO from locations.txt: " + e);
+        }
+        Assert.assertEquals(ti_expected.getId(), ti_actual.getId());
+        Assert.assertEquals(ti_expected.getName(), ti_actual.getName());
+        Assert.assertEquals(ti_expected.getLocation(), ti_actual.getLocation());
+        Assert.assertEquals(ti_expected.getNumberOfUsers(), ti_actual.getNumberOfUsers());
+        Assert.assertEquals(ti_expected.getStatus(), ti_actual.getStatus());
+        Assert.assertEquals(ti_expected.getType(), ti_actual.getType());
     }
 }
+//411711 - load_test
